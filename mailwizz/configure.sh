5855cc1f60bace3b9bd141a794dae64e46a9ab4f
Abdelhafidh Belalia <16493223+s77rt@users.noreply.github.com>
2021-06-30 16:42:19 +0100

    Updated crontab

--- mailwizz/configure.sh
+++ mailwizz/configure.sh
@@ -3,6 +3,7 @@
 # Cron
 touch mwcron
 echo "* * * * * /usr/bin/php -q /usr/share/nginx/html/apps/console/console.php send-campaigns >/dev/null 2>&1" >> mwcron
+echo "* * * * * /usr/bin/php -q /usr/share/nginx/html/apps/console/console.php queue >/dev/null 2>&1" >> mwcron
 echo "*/2 * * * * /usr/bin/php -q /usr/share/nginx/html/apps/console/console.php send-transactional-emails >/dev/null 2>&1" >> mwcron
 echo "*/10 * * * * /usr/bin/php -q /usr/share/nginx/html/apps/console/console.php bounce-handler >/dev/null 2>&1" >> mwcron
 echo "*/20 * * * * /usr/bin/php -q /usr/share/nginx/html/apps/console/console.php feedback-loop-handler >/dev/null 2>&1" >> mwcron
@@ -11,3 +12,12 @@ echo "0 * * * * /usr/bin/php -q /usr/share/nginx/html/apps/console/console.php h
 echo "0 0 * * * /usr/bin/php -q /usr/share/nginx/html/apps/console/console.php daily >/dev/null 2>&1" >> mwcron
 crontab mwcron
 rm -f mwcron
+
+/usr/bin/php -q /usr/share/nginx/html/apps/console/console.php send-campaigns >/dev/null 2>&1
+/usr/bin/php -q /usr/share/nginx/html/apps/console/console.php queue >/dev/null 2>&1
+/usr/bin/php -q /usr/share/nginx/html/apps/console/console.php send-transactional-emails >/dev/null 2>&1
+/usr/bin/php -q /usr/share/nginx/html/apps/console/console.php bounce-handler >/dev/null 2>&1
+/usr/bin/php -q /usr/share/nginx/html/apps/console/console.php feedback-loop-handler >/dev/null 2>&1
+/usr/bin/php -q /usr/share/nginx/html/apps/console/console.php process-delivery-and-bounce-log >/dev/null 2>&1
+/usr/bin/php -q /usr/share/nginx/html/apps/console/console.php hourly >/dev/null 2>&1
+/usr/bin/php -q /usr/share/nginx/html/apps/console/console.php daily >/dev/null 2>&1
